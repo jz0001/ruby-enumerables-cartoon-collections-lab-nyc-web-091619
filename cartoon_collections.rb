5536b31ca06d24714771a6eb572e39154a82e5ff
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-26 02:27:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,10 +27,3 @@ end
 # end
 potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
-def find_the_cheese(potentially_cheesy_items)
-  cheeses = %w[gouda cheddar camembert]
-
-  potentially_cheesy_items.find do |maybe_cheese|
-    cheeses.include?(maybe_cheese)
-  end
-end
